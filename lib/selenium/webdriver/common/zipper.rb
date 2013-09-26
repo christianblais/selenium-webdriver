1f0c2c93018ae170101b34ffd8a38758f9888947
Christian Blais <christ.blais@gmail.com>
2013-09-26 16:29:44 -0400

    From rubyzip to zipruby

--- lib/selenium/webdriver/common/zipper.rb
+++ lib/selenium/webdriver/common/zipper.rb
@@ -1,5 +1,5 @@
-require 'zip'
 require 'tempfile'
+require 'zipruby'
 require 'find'
 
 module Selenium
@@ -18,13 +18,20 @@ module Selenium
           destination = Dir.mktmpdir("webdriver-unzip")
           FileReaper << destination
 
-          Zip::File.open(path) do |zip|
-            zip.each do |entry|
-              to      = File.join(destination, entry.name)
-              dirname = File.dirname(to)
-
-              FileUtils.mkdir_p dirname unless File.exist? dirname
-              zip.extract(entry, to)
+          Zip::Archive.open(path) do |ar|
+            ar.each do |zf|
+              file = File.join(destination, zf.name)
+              
+              if zf.directory?
+                FileUtils.mkdir_p(file)
+              else
+                dirname = File.dirname(file)
+                FileUtils.mkdir_p(dirname) unless File.exist?(dirname)
+          
+                open(file, 'wb') do |f|
+                  f << zf.read
+                end
+              end
             end
           end
 
@@ -32,48 +39,35 @@ module Selenium
         end
 
         def zip(path)
-          with_tmp_zip do |zip|
-            ::Find.find(path) do |file|
-              unless File.directory?(file)
-                add_zip_entry zip, file, file.sub("#{path}/", '')
-              end
-            end
-
-            zip.commit
-            File.open(zip.name, "rb") { |io| Base64.strict_encode64 io.read }
-          end
-        end
-
-        def zip_file(path)
-          with_tmp_zip do |zip|
-            add_zip_entry zip, path, File.basename(path)
-
-            zip.commit
-            File.open(zip.name, "rb") { |io| Base64.strict_encode64 io.read }
-          end
-        end
-
-        private
-
-        def with_tmp_zip(&blk)
-          # can't use Tempfile here since it doesn't support File::BINARY mode on 1.8
-          # can't use Dir.mktmpdir(&blk) because of http://jira.codehaus.org/browse/JRUBY-4082
           tmp_dir = Dir.mktmpdir
           zip_path = File.join(tmp_dir, "webdriver-zip")
 
-          begin
-            Zip::File.open(zip_path, Zip::File::CREATE, &blk)
-          ensure
-            FileUtils.rm_rf tmp_dir
-            FileUtils.rm_rf zip_path
+          Zip::Archive.open(zip_path, Zip::CREATE) do |zip|
+            ::Find.find(path) do |file|
+              unless File.directory?(file)
+                zip.add_file path
+              end
+            end
+
+            File.open(path, "rb") { |io| Base64.strict_encode64 io.read }
           end
+          
+          FileUtils.rm_rf tmp_dir
+          FileUtils.rm_rf zip_path
         end
 
-        def add_zip_entry(zip, file, entry_name)
-          entry = Zip::Entry.new(zip.name, entry_name)
-          entry.follow_symlinks = true
+        def zip_file(path)
+          tmp_dir = Dir.mktmpdir
+          zip_path = File.join(tmp_dir, "webdriver-zip")
 
-          zip.add entry, file
+          Zip::Archive.open(zip_path, Zip::CREATE) do |zip|
+            zip.add_file path
+
+            File.open(path, "rb") { |io| Base64.strict_encode64 io.read }
+          end
+          
+          FileUtils.rm_rf tmp_dir
+          FileUtils.rm_rf zip_path
         end
 
       end
